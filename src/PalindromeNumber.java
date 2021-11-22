db941ba19b5f09c1a3e86fdc8d772621fefd9f3a
MelihKayali <melihkayali.35@gmail.com>
2021-11-23 00:29:24 +0300

    .

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -4,21 +4,20 @@ public class PalindromeNumber {
 		int temp = number , reverseNumber=0,lastNumber;
 		
 		while(temp !=0) {
-			System.out.println("*******************");
-			System.out.println("Number : " + temp);
 			lastNumber = temp % 10;
-			System.out.println("Last digit : " + lastNumber);
 			reverseNumber= (reverseNumber*10) + lastNumber;
-			System.out.println("New number : " + reverseNumber);
 			temp/=10;
-			System.out.println("New Temp " + temp);
 		}
+		 if(number==reverseNumber) {
+			 return true;
+		 }else {
+			 return false;
+		 }
 		
 		
-		return true;
 	}
 	public static void main(String[] args) {
-		isPalindrome(2476);
+		System.out.println(isPalindrome(4));
 
 	}
 
